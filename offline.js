c6624b0bd1dd3fb02eaaf44d6bd97aeb8a32feac
UviniW <uviniwijesinghe@gmail.com>
2019-12-03 20:49:16 +0530

    play game

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1575386255,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/zombie-sheet0.png",
+		"images/zombie-sheet1.png",
+		"images/fire-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/player2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
